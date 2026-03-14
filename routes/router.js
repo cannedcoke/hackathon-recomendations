8c0e79c082640f37f0fb98616473f82499906bed
Cyanide <miqueas736d@gmail.com>
2026-03-14 17:30:27 -0300

    feat:  agregando carpeta para rutas

---
+++ routes/router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+// const controller = require("../controllers/loginController");
+// const controller = require("../controllers/mainPage");
+// const controller = require("../controllers/signInController");
+// const controller = require("../controllers/userFormController");
+
+
+// defino api endpoints y le indico que funcion del controlador usar
+
+
+// hacer rutas para 
+// router.get("/", controller.showData);
+// router.post("/add", controller.addRecord);
+// router.post("/vote",controller.addVote)
+// router.delete("/unVote",controller.removeVote)
+// router.put("/update",controller.updateRecord)
+// exporto para poder acceder externamente
+module.exports = router;
