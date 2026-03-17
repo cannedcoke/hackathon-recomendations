24911ade0d202433d05b680e76dfb4d3d8e854d2
Cyanide <miqueas736d@gmail.com>
2026-03-17 12:44:12 -0300

    fix: arreglando logica de renderizado

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -5,12 +5,20 @@ UserData.addEventListener("submit", async (e) => {
     const email = document.getElementById("nombre").value
     const password = document.getElementById("password").value
 
-    await fetch("/signIn",{
+    const response = await fetch("/signIn",{
         method:"POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
 
     })
-    location.reload();
+        if (!response.ok) {
+        const data = await response.json();
+        alert(data.message);
+        return;
+    }
+
+        // Si el servidor respondió bien, recargar a la URL que devolvió
+    const data = await response.json();
+    window.location.href = data.redirect;
 
 })
