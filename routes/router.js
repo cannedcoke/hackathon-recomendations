66e185e236935b0801ff1bf369235e92f57de530
Cyanide <miqueas736d@gmail.com>
2026-03-17 12:41:48 -0300

    feat: agregando rutas para main page

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const LoginController = require("../controllers/loginController");
 const SignInController = require("../controllers/signInController");
+// const mainPageController = require("../controllers/mainPageController");
 
 
 
@@ -11,6 +12,7 @@ const SignInController = require("../controllers/signInController");
 // hacer rutas para 
 router.post("/login", LoginController.login);
 router.post("/signIn", SignInController.signIn);
+// router.post("/recommendations", mainPageController.ponerFuncion aca);
 
 router.get("/userForm", (req, res) => res.render("userForm"));
 router.get("/mainPage", (req, res) => res.render("mainPage"));
